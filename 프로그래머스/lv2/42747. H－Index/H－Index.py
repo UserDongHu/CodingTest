962f2b4c811519dbeda225c07f4571c04826faf3
SadMe <137512514+UserDongHu@users.noreply.github.com>
2023-08-12 19:34:59 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,12 @@
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+    for i in range(len(citations), 0, -1):
+        count = 0
+        for j in citations:
+            if j >= i:
+                count += 1
+        if count >= i:
+            answer = i
+            break
+    return answer
